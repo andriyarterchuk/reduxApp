e44c2700274a1496c9aac34c567cc42fd0152d02
Andriy Arterchuk <andriyarterchuk@MacBook-Pro-Andriy.local>
2018-09-09 20:07:17 +0300

    add google map

--- src/pages/Main/components/Main.jsx
+++ src/pages/Main/components/Main.jsx
@@ -3,18 +3,36 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import openSocket from 'socket.io-client';
 import '../../../App.css';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 
 import {
   getCoordinates
 } from '../actions';
 
 let socket;
+const MapWithMarkers = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={5}
+    defaultCenter={{ lat: 50.4547, lng: 30.5238 }}
+  >
+  {props.coordinates.map( ({lat, long}, index) => (
+    <Marker
+      key={index}
+      position={{ lat, lng: long }}
+    />
+  ))}
+  </GoogleMap>
+));
 
 class Main extends Component {
   componentDidMount() {
     socket = openSocket('http://localhost:3000');
     socket.on('coordinates', coordinates => {
-      console.log('coordinates', coordinates)
       this.props.getCoordinates(coordinates)
     });
     socket.emit('fetchCoordinates');
@@ -23,12 +41,13 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <h3>Coordinates</h3>
-        {this.props.coordinates.map((item, index) => (
-          <div key={index}>
-           {`${index + 1}) latitude: ${item.lat}, longitude: ${item.long}`}
-          </div>
-        ))}
+        <MapWithMarkers
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100vh` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          coordinates={this.props.coordinates}
+        />
       </div >
     );
   }
